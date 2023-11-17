c4bc94be0c813000a11c529a1741d25110e25e08
Alexander Kiemle <mail@alexander-kiemle.de>
2023-11-17 16:44:15 +0100

    add openapi 3 support (swagger-ui)

--- src/main/java/de/kiemle/notes/controller/Controller.java
+++ src/main/java/de/kiemle/notes/controller/Controller.java
@@ -1,11 +1,13 @@
-package de.kiemle.notes;
+package de.kiemle.notes.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
+@RequestMapping("/api")
 public class Controller {
 
     @GetMapping("/hello")
